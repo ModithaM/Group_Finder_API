6590e0819a8e7c5c1d9adef1af8c99e8edc5ff48
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-06-17 01:00:06 +0530

    feat: validation for api params

---
+++ src/main/java/com/moditha/group_finder/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.moditha.group_finder.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //handle invalid inputs coming from the client
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
